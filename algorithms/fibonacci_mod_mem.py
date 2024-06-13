7499f861077b3f70672e990130a31499211f625d
Matthias_Milczynski <matthias.milczynski@gmail.com>
2024-06-12 18:20:02 -0700

    Algo adaptations and aditions for debugging.

--- algorithms/fibonacci_mod_mem.py
+++ algorithms/fibonacci_mod_mem.py
@@ -4,10 +4,12 @@ def fibonacci_mod_m_mem(n, m):
     y_0 = 0
     y_1 = 1
     y_2 = 1
-    for i in range(2, n):
+    i = 2
+    while i < n:
         y_0 = y_1
         y_1 = y_2
         y_2 = y_1 + y_0
+        i += 1
     return y_2 % m
 
 
@@ -15,9 +17,13 @@ def fibonacci_mod_m(n, m):
     y = [0] * (n + 1)
     y[0] = 0
     y[1] = 1
+    mods = [0] * (n + 1)
+    mods[0] = y[0] % m
+    mods[1] = y[1] % m
     for i in range(2, n + 1):
         y[i] = y[i - 1] + y[i - 2]
-    return y[n] % m
+        mods[i] = y[i] % m
+    return y, mods
 
 
 if __name__ == "__main__":
--- algorithms/last_digit_fibonacci.py
+++ algorithms/last_digit_fibonacci.py
@@ -1,7 +1,6 @@
 def last_digit_fibonacci(n):
     if n == 0:
         return 0
-    # y = [0] * (n + 1) -> will result in memory error for high n
     y_0 = 0
     y_1 = 1
     y_2 = 1
@@ -11,7 +10,6 @@ def last_digit_fibonacci(n):
         y_1 = y_2
         y_2 = y_1 + y_0
         i += 1
-        # y[i] = y[i - 1] + y[i - 2]
     return y_2 % 10
 
 
